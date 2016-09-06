f9b11790efcf4c1671639763ecbf0d685127359a
Adam Rice <adamcoltonrice@gmail.com>
2016-09-06 16:40:30 -0600

    random btn functionality added

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-var $random = $('#random')
-var $reset = $('#reset')
+// var $random = $('#random');
+// var $reset = $('#reset');
+// var $content = $('#content');
+
+$(document).ready(function() {
+    $("#random").click(function() {
+        $("body").css("backgroundColor", getRandomColor() );
+    });
+});
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
